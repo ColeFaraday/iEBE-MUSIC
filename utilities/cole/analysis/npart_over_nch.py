0b3df8a6edd69f01e4ac5f814cff7ea973b97c75
ColeFaraday <colefaraday@gmail.com>
2025-06-25 16:20:42 +0200

    Calculate mean pT

--- utilities/cole/analysis/npart_over_nch.py
+++ utilities/cole/analysis/npart_over_nch.py
@@ -18,15 +18,17 @@ def help_message():
     print(f"Usage: {sys.argv[0]} input_file.h5 output_file.txt")
     sys.exit(1)
 
-def calculate_yield(pT_low, pT_high, data):
+def calculate_yield_and_meanpTsq(pT_low, pT_high, data):
     npT = 500
     pT_array = np.linspace(pT_low, pT_high, npT)
     dpT = pT_array[1] - pT_array[0]
     pT_event = data[:, 0]
     dN_event = data[:, 1]
     dN_interp = np.exp(np.interp(pT_array, pT_event, np.log(dN_event + 1e-30)))
+    meanpTsq = (np.sum(dN_interp*pT_inte_array**2.)
+              / np.sum(dN_interp*pT_inte_array))
     N = 2. * np.pi * np.sum(dN_interp * pT_array) * dpT
-    return N
+    return N, meanpTsq
 
 if len(sys.argv) != 3:
     help_message()
@@ -96,16 +98,19 @@ with open(output_file, "w") as fout:
         Npart = np.count_nonzero(npart_data[:, 3] == 1)
 
         Nch_list = []
+        meanpTsq_list = []
         for exp, cuts in kinematicCutsDict.items():
             vn_key = f"particle_9999_vndata_diff_eta_{cuts['etamin']}_{cuts['etamax']}.dat"
             try:
                 vn_data = np.nan_to_num(group[vn_key])
-                Nch = calculate_yield(cuts["pTmin"], cuts["pTmax"], vn_data)
+                Nch, meanpTsq = calculate_yield_and_meanpTsq(cuts["pTmin"], cuts["pTmax"], vn_data)
             except KeyError:
                 Nch = -1.0
+                meanpTsq = -1
             Nch_list.append(Nch)
+            meanpTsq_list.append(meanpTsq)
 
-        row = f"{id:<7d}  {centMin:<7.2f}  {centMax:<7.2f}  {Ncoll:<6d}  {Npart:<6d}  " + "  ".join(f"{nch:.4e}" for nch in Nch_list) + "\n"
+        row = f"{id:<7d}  {centMin:<7.2f}  {centMax:<7.2f}  {Ncoll:<6d}  {Npart:<6d}  " + "  ".join(f"{nch:.4e}" for nch in Nch_list) + "  ".join(f"{mpt:.4e}" for mpt in meanpTsq_list)+ "\n"
         fout.write(row)
 
 h5_data.close()
