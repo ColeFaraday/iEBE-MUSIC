14908241521667a15b8f961998380ae74c368162
ColeFaraday <colefaraday@gmail.com>
2025-07-09 12:22:48 +0200

    Allow `get_Qnvectors.sh` to run in any folder

--- utilities/get_Qnvectors.sh
+++ utilities/get_Qnvectors.sh
@@ -3,11 +3,16 @@
 evfolder=${1%"/"}
 database=${2%"/"}
 
-for iev in `ls --color=none ${evfolder}`
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for iev in $(ls --color=none "${evfolder}")
 do
-    eventid=`echo $iev | sed 's/hydro_results_//' | sed 's$/$$'`
-    python3 fetch_Qnvectors_from_hdf5_database.py $database $eventid
-    mv Qn_vectors_${eventid}.dat ${evfolder}/$iev/
-    mv particle_yield_and_meanpT_${eventid}.dat ${evfolder}/$iev/
-    mv Ncoll* ${evfolder}/$iev/
+    eventid=$(echo "$iev" | sed 's/hydro_results_//' | sed 's$/$$')
+    
+    python3 "${SCRIPT_DIR}/fetch_Qnvectors_from_hdf5_database.py" "$database" "$eventid"
+    
+    mv "Qn_vectors_${eventid}.dat" "${evfolder}/$iev/"
+    mv "particle_yield_and_meanpT_${eventid}.dat" "${evfolder}/$iev/"
+    mv Ncoll* "${evfolder}/$iev/"
 done
