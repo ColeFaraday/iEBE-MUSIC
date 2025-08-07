83f7f71bd8f6e15a739db0cdea2906ce4196ac49
ColeFaraday <colefaraday@gmail.com>
2025-08-07 12:05:54 +0200

    chore: debug prints

--- utilities/combine_multiple_hdf5.py
+++ utilities/combine_multiple_hdf5.py
@@ -50,6 +50,9 @@ for ievent, event_path in enumerate(EVENT_LIST):
         if gtemp2 != gtemp:
             print("Conflict in mergeing {0}, use {1}".format(gtemp, gtemp2))
         exist_group_keys.append(gtemp2)
+        print("going to run this verbatim command: \n"
+              "h5copy -i {0} -o {1}.h5 -s {2} -d {3}".format(
+                  event_path, RESULTS_NAME, gtemp, gtemp2))
         system('h5copy -i {0} -o {1}.h5 -s {2} -d {3}'.format(
             event_path, RESULTS_NAME, gtemp, gtemp2))
 
